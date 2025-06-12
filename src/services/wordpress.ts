f7b8fe90b9c4d8f3510ab6c44a495a9f3c9f38f7
oredytech <sonybabaoredy@gmail.com>
2025-06-12 18:06:18 +0200

    Update wordpress.ts

--- src/services/wordpress.ts
+++ src/services/wordpress.ts
@@ -327,10 +327,10 @@ export const getFeaturedImageUrl = (post: WordPressPost): string => {
     console.log('Post featured_media ID:', post.featured_media);
     
   //   // Return default image when no featured image is available
-       return 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=800&q=80';
+       return 'public/lovable-uploads/placeholder.png';
        } catch (error) {
          console.error("Error getting featured image URL:", error);
-         return 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=800&q=80';
+         return 'public/lovable-uploads/placeholder.png';
        }
 };
 
