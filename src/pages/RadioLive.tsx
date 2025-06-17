182ec33e71a1472f8d2307f22d84289892508a7a
oredytech <sonybabaoredy@gmail.com>
2025-06-17 17:11:28 +0200

    Update RadioLive.tsx

--- src/pages/RadioLive.tsx
+++ src/pages/RadioLive.tsx
@@ -92,8 +92,6 @@ const RadioLive = () => {
               <h3 className="font-medium mb-2">Comment nous écouter</h3>
               <ul className="list-disc pl-5 text-sm text-muted-foreground space-y-1">
                 <li>En direct sur ce site web</li>
-                <li>Sur les fréquences FM: 95.5 MHz (Goma), 94.3 MHz (Bukavu)</li>
-                <li>Sur notre application mobile (Android & iOS)</li>
                 <li>Sur certains agrégateurs de radios en ligne</li>
               </ul>
             </div>
@@ -109,15 +107,15 @@ const RadioLive = () => {
               <ul className="text-sm space-y-1">
                 <li className="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
-                  <span>+243 999 999 999</span>
+                  <span>+243 971 121 702</span>
                 </li>
                 <li className="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m4 6 8 5 8-5"/><path d="M3 6a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>
-                  <span>radio@mishapivoice.tv</span>
+                  <span>mishapivoicetv.adg@gmail.com</span>
                 </li>
                 <li className="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"/></svg>
-                  <span>@MishapiVoice</span>
+                  <span>@MishapiVoiceTv</span>
                 </li>
                 <li className="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
